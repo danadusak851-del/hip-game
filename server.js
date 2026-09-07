e9f279d10db9d09d896ec9338388e803369a9f82
danadusak851-del <danadusak851@gmail.com>
2026-09-07 09:54:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(__dirname));
 const rooms = {};
 
 const suits = ['♠', '♥', '♦', '♣'];
